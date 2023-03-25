f3bece2c8be4dcb6fa839208c8b0e3b26d1869c9
AnBoondo <do0344@naver.com>
2023-03-25 10:50:50 +0900

    다차원배열

--- 2023_03_25/.cpp
+++ 2023_03_25/.cpp
@@ -2,31 +2,23 @@
 
 
 int main() {
-	int num = 0; // 변수의 선언과 초기화가 동시에
-	int num1; //변수의 선언
-	num1 = 0; //변수의 초기화
-	std::string str = "";
-
-	std::string fruit[3] = { "apple", "banana", "grape" };
-	//::string fruit[] = { "apple", "banana", "grape" };//오른쪽에 3개 지정해줘서 굳이 []안에 3이라고 안써도 됨
-	//선언할 때 크기가 정해져야 함
-
-	std::cout << fruit << std::endl;
-	std::cout << fruit[0] << std::endl;
-	std::cout << fruit[1] << std::endl;
-	std::cout << fruit[2] << std::endl;
-
-	std::string fruit1[3]; //배열의 선언
-	//인덱스는 무조건 0부터 시작한다 그래서 0이 첫번째
-	fruit1[0] = "apple";
-	fruit1[1] = "banana";
-	fruit1[2] = "grape";
+	// 2 2 2
+	// 2 23 1
+	int num[2][3] = { {2, 2, 2}, 
+		{2, 23, 1} }; //[첫번째는 행2] [두번째는 열3]
+	int i, j;
 	
-	//sizeof() : 변수, 자료형의 크기를 byte 단위로 만들어준다
-	//예를 들어 int는 4byte인데 3개있으면 sizeof()이거는 12가 나온다.
-	for (int i = 0; i < sizeof(fruit)/sizeof(fruit[0]); i++) {
-		//sizeof(fruit)는 전체 / sizeof(fruit[0])는 하나짜리
-		std::cin >> fruit[i];
-		std::cout << fruit[i] << std::endl;
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 3; j++) {
+			std::cout << num[i][j] << "";
+		}
+		std::cout << std::endl;
 	}
+	std::cout << i;
+	std::cout << j;
+	// a b c d
+	//e f g h
+	std::string alpha[2][4] = { {"a","b","c","d"},{"e","f","g","h"} };
+	
+
 }
