7086a01b84ce1d9f1450fdfb064dafba2b3c0549
AnBoondo <do0344@naver.com>
2023-05-11 18:22:08 +0900

    chat2

--- Chat2/Chat2/Server.cpp
+++ Chat2/Chat2/Server.cpp
@@ -1,7 +1,5 @@
-
-#pragma comment(lib, "ws2_32.lib") // 명시적으로 라이브러리를 호출하는 방법 중 하나.
-
-#include <WinSock2.h> //winsock 헤더 사용중
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h> 
 #include <string>
 #include <iostream>
 #include <thread>
@@ -11,7 +9,7 @@
 
 
 #define MAX_SIZE 1024
-#define MAX_CLIENT 4 // 최대 3명
+#define MAX_CLIENT 10
 
 using std::cout;
 using std::cin;
@@ -23,32 +21,31 @@ bool is_login;
 
 
 struct SOCKET_INFO {
-    SOCKET sck; // ctrl + 클릭 하면 헤더 속 SOCKET 코드를 찾아갈 수 있다, socket은 unsigned int pointer를 갖고 있구나 하고 예측할 수 있다.
-    string user; // user : 사람의 이름
+    SOCKET sck;
+    string user;
+    string id;
 };
 
-std::vector<SOCKET_INFO> sck_list; // 서버에 연결된 client 저장할 변수
-SOCKET_INFO server_sock; // 서버 소켓의 정보를 저장할 변수
-int client_count = 0;  //현재 접속하고 있는 클라이언트의 수 카운트.
+std::vector<SOCKET_INFO> sck_list; 
+SOCKET_INFO server_sock; 
+int client_count = 0;  
 string nickname;
 
-void server_init(); // 서버용 소켓을 만드는 함수. ~ listen()
-void add_client(); // accept 함수 실행되고 있을 예fh
-void add_client_retry(SOCKET_INFO new_client, char* buf);
-void send_msg(const char* msg); // send () 실행
-void recv_msg(int idx); // recv() 실행
-void del_client(int idx); // 클라이언트와의 연결을 끊을 때
+void server_init();
+void add_client();
+void add_client_authentication(SOCKET_INFO new_client, char* buf);
+void send_msg(const char* msg);
+void send_history(string user_id, SOCKET_INFO new_client);
+void recv_msg(int idx);
+void del_client(int idx);
 void sql_signup(string id, string nickname, string password);
-void sql_is_id_duplicate(string new_id);
-void check_id_duplicate(string new_id);
+void sql_check_if_id_exists(string new_id);
 void sql_connect();
 void sql_disconnect();
-void sql_login(string id, string pw);
+string sql_login(string id, string pw);
 void sql_message_input(string sender, string receiver, string msg);
-void print_chat_history(string nickname);
 
-// MySQL Connector/C++ 초기화
-sql::mysql::MySQL_Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
+sql::mysql::MySQL_Driver* driver; 
 sql::Connection* con;
 sql::Statement* stmt;
 sql::PreparedStatement* pstmt;
@@ -58,47 +55,68 @@ int main() {
     WSADATA wsa;
     int code = WSAStartup(MAKEWORD(2, 2), &wsa); 
     if (!code) {
-        server_init(); // 서버측 소켓 활성화
-
+        server_init(); 
         std::thread th1[MAX_CLIENT];
- 
         for (int i = 0; i < MAX_CLIENT; i++) {
             th1[i] = std::thread(add_client);
         }
-
-
         while (1) {
             string text, msg = "";
-
+            string is_command, command;
             std::getline(cin, text);
             const char* buf = text.c_str();
-            msg = server_sock.user + " : " + buf;
-            send_msg(msg.c_str());
-        }
+            std::stringstream serverstream(buf);
+            serverstream >> is_command;
+            if (is_command == "admin") {
+                serverstream >> command;
+                if (command == "ban") {
+                    string ban_who;
+                    serverstream >> ban_who;
+                    for (int i = 0; i < MAX_CLIENT; i++) {
+                        if (sck_list[i].id == ban_who) {
+                            send(sck_list[i].sck, ("서버 :" +sck_list[i].user + " 너 밴").c_str(), MAX_SIZE, 0);
+                            del_client(i);
+                        }
+                    }
+                }
+                else if (command == "whisper") {
+                    string whisper_who;
+                    serverstream >> whisper_who;
+                    for (int i = 0; i < MAX_CLIENT; i++) {
+                        if (sck_list[i].id == whisper_who) {
+                            string message;
+                            std::getline(serverstream, message);
+                            message.erase(message.begin());
+                            send(sck_list[i].sck, ("서버 :" + message).c_str(), MAX_SIZE, 0);
+              
+                        }
+                    }
+                    }
+                else if (command == "decrease") {}
+            }
+            else{ msg = server_sock.user + " : " + buf;
+                send_msg(msg.c_str());}
+               
 
+        }
         for (int i = 0; i < MAX_CLIENT; i++) {
             th1[i].join();
         }
-
         closesocket(server_sock.sck); 
     }
     else {
         cout << "프로그램 종료. (Error code : " << code << ")"; 
     }
-
     WSACleanup();
-
-
-    return 0;
+    0;
 }
 
 void server_init() {
     server_sock.sck = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);  
     SOCKADDR_IN server_addr = {};
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2304); // ~~~~:7777
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //  127.0.0.1: ~~~~
-
+    server_addr.sin_port = htons(2304); 
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
     bind(server_sock.sck, (sockaddr*)&server_addr, sizeof(server_addr));
     listen(server_sock.sck, SOMAXCONN);
     server_sock.user = "서버";
@@ -106,109 +124,110 @@ void server_init() {
 }
 
 void add_client() {
-    cout << "add_client" << endl;
     SOCKADDR_IN addr = {};
     int addrsize = sizeof(addr);
-    char buf[MAX_SIZE] = { }; // 메시지 최대 길이 설정
+    char buf[MAX_SIZE] = { };
 
-    ZeroMemory(&addr, addrsize); //  addr 메모리를 0x00으로 초기화/addrsize = sizeof(addr), 
-
+    ZeroMemory(&addr, addrsize);
     SOCKET_INFO new_client = {};
     new_client.sck = accept(server_sock.sck, (sockaddr*)&addr, &addrsize);
-            add_client_retry(new_client, buf);
 
+    while (1) {
+        add_client_authentication(new_client, buf);
+        if (is_login || !is_duplicate) {
+            break;
+        }
+        closesocket(new_client.sck);
+        new_client.sck = accept(server_sock.sck, (sockaddr*)&addr, &addrsize);
+    }
 }
-void add_client_retry(SOCKET_INFO new_client, char* buf) {
-    recv(new_client.sck, buf, MAX_SIZE, 0); 
-    std::stringstream userinfo(buf);
-    string id, pw;
-    userinfo >> new_client.user >> id >> pw;
 
+void add_client_authentication(SOCKET_INFO new_client, char* buf) {
+    recv(new_client.sck, buf, MAX_SIZE, 0);
+    std::stringstream userinfo(buf);
+    string id, pw, temp;
+    userinfo >> new_client.user >> id >> pw;
+    new_client.id = id;
     if (new_client.user == "*login*") {
-        sql_login(id, pw);
+        temp = sql_login(id, pw);
         if (is_login) {
-            send(new_client.sck, "Success : 로그인을 성공했습니다.", MAX_SIZE, 0);
-            new_client.user = nickname; 
+            nickname = temp;
+            send(new_client.sck, ("Success " + nickname + " 로그인을 성공했습니다.").c_str(), MAX_SIZE, 0);
+            send_history(id, new_client);
+            new_client.user = nickname;
+            string msg = "[공지] " + new_client.user + " 님이 입장했습니다.";
+            cout << msg << endl;
+            sck_list.emplace_back(new_client);
+            std::thread th(recv_msg, client_count);
+            client_count++;
+            cout << "[공지] 현재 접속자 수 : " << client_count << "명" << endl;
+            send_msg(msg.c_str());
+            th.join();
+
+        }
+        else {
+            send(new_client.sck, "Fail : 아이디 혹은 패스워드가 다릅니다.", MAX_SIZE, 0);
+            cout << "Fail : 아이디 혹은 패스워드가 다릅니다." << endl;
+        }
+    }
+    else {
+        is_duplicate = false;
+        sql_check_if_id_exists(id);
+        if (!is_duplicate) {
+            sql_signup(id, new_client.user, pw);
+            send(new_client.sck, "Success : 회원가입에 성공했습니다.", MAX_SIZE, 0);
             string msg = "[공지] " + new_client.user + " 님이 입장했습니다.";
             cout << msg << endl; 
-            stmt = con->createStatement();
-            stmt->execute("set names euckr");
-            if (stmt) { delete stmt; stmt = nullptr; }
-            print_chat_history(new_client.user);
 
-            sck_list.push_back(new_client); 
-
-            std::thread th(recv_msg, client_count);
-
+            sck_list.emplace_back(new_client);
+            std::thread th(recv_msg, client_count); 
             client_count++; 
             cout << "[공지] 현재 접속자 수 : " << client_count << "명" << endl;
             send_msg(msg.c_str());
             th.join(); 
         }
         else {
-            send(new_client.sck, "Fail : 아이디 혹은 패스워드가 다릅니다.", MAX_SIZE, 0);
-            cout << "Fail : 아이디 혹은 패스워드가 다릅니다." << endl;
-            add_client_retry(new_client, buf);
-        }
-    }
-    else {
-        is_duplicate = false;
-        sql_is_id_duplicate(id);
-        if (!is_duplicate) {
-
-            sql_signup(id, new_client.user, pw);
-            send(new_client.sck, "Success : 회원가입에 성공했습니다.", MAX_SIZE, 0);
-            string msg = "[공지] " + new_client.user + " 님이 입장했습니다.";
-            cout << msg << endl;
-            sck_list.push_back(new_client); 
-            std::thread th(recv_msg, client_count); 
-            client_count++; 
-            cout << "[공지] 현재 접속자 수 : " << client_count << "명" << endl;
-            send_msg(msg.c_str());
-            th.join();
-        }
-        else {
             cout << "duplicated" << endl;
             send(new_client.sck, "Fail : 중복된 아이디가 있습니다.", MAX_SIZE, 0);
-            add_client_retry(new_client, buf);
+            add_client_authentication(new_client, buf);
         }
     }
 }
 
 void send_msg(const char* msg) {
-    for (int i = 0; i < client_count; i++) {
-        send(sck_list[i].sck, msg, MAX_SIZE, 0);
+    for (const auto& client : sck_list) {
+        send(client.sck, msg, MAX_SIZE, 0);
     }
-
-}
-void send_msg(SOCKET sck, string msg) {
 }
 
 void recv_msg(int idx) {
     char buf[MAX_SIZE] = { };
     string msg = "";
-
     while (1) {
-        ZeroMemory(&buf, MAX_SIZE); // buf 0으로 초기화
+        ZeroMemory(&buf, MAX_SIZE);
         if (recv(sck_list[idx].sck, buf, MAX_SIZE, 0) > 0) {
             std::stringstream sss(buf);
-            string this_id, receiver, message, temp;
+            string this_id, is_whisper, receiver, message;
             sss >> this_id;
-            sss >> message;
-            if (message == "to:" or message == "TO:" or message == "to" or message == "TO") {
-                sss >> receiver;
-                getline(sss, message);
-            }
-            else {
-                receiver = "*";
-                std::stringstream ssss(buf);
-                ssss >> this_id;
-                getline(ssss, message);
-            }
-            cout << this_id << " " << receiver << " " << message << " " << endl;
+            sss >> is_whisper;
+            sss >> receiver;
+            std::getline(sss, message);
+            message.erase(message.begin());
             msg = sck_list[idx].user + " : " + message;
             cout << msg << endl;
-            send_msg(msg.c_str());
+
+            if (is_whisper == "Y") {
+                for (int i = 0; i < MAX_CLIENT; i++) {
+                    if (sck_list[i].id == receiver) {
+                        send(sck_list[i].sck, msg.c_str(), MAX_SIZE, 0);
+                        break;
+                    }
+                }
+            }
+            else {
+                send_msg(msg.c_str());
+            }
+
             sql_message_input(this_id, receiver, message);
         }
         else {
@@ -222,24 +241,61 @@ void recv_msg(int idx) {
 }
 
 
+void send_history(string user_id, SOCKET_INFO new_client) {
+    try {
+        driver = sql::mysql::get_mysql_driver_instance();
+        con = driver->connect(server, username, dbpassword);
+        con->setSchema("project_chat");
+        stmt = con->createStatement();
+        stmt->execute("set names euckr");
+        sql::PreparedStatement* pstmt = con->prepareStatement("SELECT * FROM message WHERE (sender = ? OR receiver = ?) OR (receiver = '*')");
+        pstmt->setString(1, user_id);
+        pstmt->setString(2, user_id);
+        sql::ResultSet* res = pstmt->executeQuery();
+        while (res->next()) {
+            string message = res->getString("message");
+            string time = res->getString("time");
+            string sender = res->getString("sender");
+            string green = "\x1B[32m"; 
+            string yellow = "\x1B[33m"; 
+            string blue = "\x1B[34m"; 
+            string magenta = "\x1B[35m"; 
+            string cyan = "\x1B[36m";
+            string bold = "\x1B[1m";
+            string reset = "\x1B[0m"; 
+            string output = "[" + yellow + time + reset + "] " +
+                "[" + green + sender + reset + "] " +
+                bold + reset + ": " + message;
+            send(new_client.sck, output.c_str(), MAX_SIZE, 0);
+        }
+        delete res;
+        delete pstmt;
+    }
+    catch (sql::SQLException& e) {
+        cout << "SQL Exception: " << e.what() << endl;
+    }
+    if (stmt) { delete stmt; stmt = nullptr; }
+}
+
 void del_client(int idx) {
     closesocket(sck_list[idx].sck);
     client_count--;
 }
 void sql_signup(string id, string nickname, string password) {
-   sql_connect();
-    con->setSchema("project_chat"); 
+    sql_connect();
+    con->setSchema("project_chat");
     stmt = con->createStatement();
     stmt->execute("set names euckr");
     if (stmt) { delete stmt; stmt = nullptr; }
-    pstmt = con->prepareStatement("INSERT INTO user(id, nickname, password) VALUES(?,?,?)"); // INSERT
+    pstmt = con->prepareStatement("INSERT INTO user(id, nickname, password) VALUES(?,?,?)"); 
     pstmt->setString(1, id);
     pstmt->setString(2, nickname);
     pstmt->setString(3, password);
-    pstmt->execute(); // 쿼리 실행
+    pstmt->execute(); 
     sql_disconnect();
+
 }
-void sql_is_id_duplicate(string new_id) {
+void sql_check_if_id_exists(string new_id) {
     sql_connect();
     stmt = con->createStatement();
     result = stmt->executeQuery("SELECT * FROM user WHERE id = '" + new_id + "'");
@@ -247,21 +303,20 @@ void sql_is_id_duplicate(string new_id) {
     while (result->next()) {
         cout << "중복검사에 걸림" << endl;
         is_duplicate = true;
-        sql_disconnect();
     }
-
+    sql_disconnect();
 }
-
 void sql_connect() {
     try {
         driver = sql::mysql::get_mysql_driver_instance();
-        con = driver->connect(server, username, dbpassword); // MySQL 데이터베이스 연결 객체
+        con = driver->connect(server, username, dbpassword); 
     }
     catch (sql::SQLException& e) {
         cout << "Could not connect to server. Error message: " << e.what() << endl;
         exit(1);
     }
     con->setSchema("project_chat");
+
     stmt = con->createStatement();
     stmt->execute("set names euckr");
     if (stmt) { delete stmt; stmt = nullptr; }
@@ -277,7 +332,7 @@ void sql_disconnect() {
     }
 }
 
-void sql_login(string id, string pw) {
+string sql_login(string id, string pw) {
     sql_connect();
     is_login = false;
     pstmt = con->prepareStatement("SELECT * FROM user WHERE id = ? AND password = ?");
@@ -293,8 +348,8 @@ void sql_login(string id, string pw) {
         result = pstmt->executeQuery();
         while (result->next()) {
             nickname = result->getString(1);
-            cout << nickname << endl;
             is_login = true;
+            return nickname;
         }
     }
     sql_disconnect();
@@ -305,37 +360,11 @@ void sql_message_input(string sender, string receiver, string message) {
     con->setSchema("project_chat");
     stmt = con->createStatement();
     stmt->execute("set names euckr");
-   if (stmt) { delete stmt; stmt = nullptr; }
+    if (stmt) { delete stmt; stmt = nullptr; }
     pstmt = con->prepareStatement("INSERT INTO message(sender, receiver, message) VALUES(?,?,?);");
-
     pstmt->setString(1, sender);
     pstmt->setString(2, receiver);
     pstmt->setString(3, message);
-
     pstmt->execute(); 
     sql_disconnect();
-}
-
-void print_chat_history(string nickname) {
-    try {
-        driver = sql::mysql::get_mysql_driver_instance();
-        con = driver->connect(server, username, dbpassword);
-        con->setSchema("project_chat");
-        sql::PreparedStatement* pstmt = con->prepareStatement("SELECT * FROM message");
-        sql::ResultSet* res = pstmt->executeQuery();
-        while (res->next()) {
-            string sender = res->getString("sender");
-            string message = res->getString("message");
-            string time = res->getString("time");
-            cout << "[" << time << "] " << sender << ": " << message << endl;
-        }
-        delete res;
-        delete pstmt;
-    }
-    catch (sql::SQLException& e) {
-        cout << "SQL Exception: " << e.what() << endl;
-    }
-    stmt = con->createStatement();
-    stmt->execute("set names euckr");
-    if (stmt) { delete stmt; stmt = nullptr; }
 }
---
+++ Chat2/User2/Client2.cpp
@@ -0,0 +1,165 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h> 
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <thread>
+#include <unordered_map>
+
+#define MAX_SIZE 1024
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+SOCKET client_sock;
+string my_nick;
+string id, password;
+bool isServerConnected = false;
+bool is_login;
+
+std::unordered_map<string, string> user_colors;
+int color_index = 0;
+
+int chat_recv() {
+    char buf[MAX_SIZE] = {};
+    string msg;
+    while (isServerConnected) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);
+            string user;
+            ss >> user;
+            if (user_colors.find(user) == user_colors.end()) {
+                user_colors[user] = "\033[3" + std::to_string(color_index) + "m";
+                color_index = (color_index + 1) % 10;
+            }
+
+            if (user != my_nick)
+                cout << user_colors[user] << buf << "\033[0m" << endl;
+        }
+        else {
+            cout << "서버와의 연결이 끊어졌습니다." << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
+int result_recv() {
+    char buf[MAX_SIZE] = {};
+    string msg;
+    while (isServerConnected) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);
+            string result;
+            ss >> result;
+            if (result == "Success") {
+                is_login = true;
+                my_nick = "";
+                ss >> my_nick;
+            }
+            break;
+        }
+        else {
+            cout << "서버와의 연결이 끊어졌습니다." << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    WSADATA wsa;
+    int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+    client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKADDR_IN client_addr = {};
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(2304);
+    InetPton(AF_INET, TEXT("127.0.0.1"), &client_addr.sin_addr);
+
+    while (1) {
+        if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) {
+
+            break;
+        }
+        cout << "Connecting..." << endl;
+    }
+    isServerConnected = true;
+
+    if (!code) {
+        cout << "이미 아이디가 있으시면 1, 회원가입을 원하시면 2번을 눌러주세요.";
+        int sign;
+        cin >> sign;
+        if (sign == 1) {
+            my_nick = "*login*";
+            cout << "로그인을 해주세요" << endl;
+
+            while (true) {
+                cout << "아이디를 입력해주세요 : ";
+                cin >> id;
+                cout << "비밀번호를 입력해주세요 : ";
+                cin >> password;
+
+                send(client_sock, (my_nick + ' ' + id + ' ' + password).c_str(), (my_nick + ' ' + id + ' ' + password).length(), 0);
+                result_recv();
+                if (is_login) {
+                    break;
+                }
+            }
+        }
+        else if (sign == 2) {
+            cout << "회원가입 페이지입니다." << endl;
+            while (true) {
+                cout << "아이디를 입력해주세요 :";
+                cin >> id;
+                cout << "비밀번호를 입력해주세요 :";
+                cin >> password;
+                cout << "사용할 닉네임 입력 : ";
+                cin >> my_nick;
+                send(client_sock, (my_nick + ' ' + id + ' ' + password).c_str(), (my_nick + ' ' + id + ' ' + password).length(), 0);
+                result_recv();
+                if (is_login) break;
+            }
+        }
+
+        std::thread th2(chat_recv);
+        while (1) {
+            string text, temp, receiver;
+            const char* buffer;
+            std::getline(cin, text);
+
+
+            std::stringstream text_inspect(text);
+            int word_count = 0;
+            bool is_whisper = false;
+            while (text_inspect >> temp) {
+                if (temp == "to:" or temp == "TO:" or temp == "to" or temp == "TO") {
+                    is_whisper = true;
+                    text_inspect >> receiver;
+                    text = "";
+                    std::getline(text_inspect, text);
+                }
+                else {
+                    break;
+                }
+            }
+            if (is_whisper) {
+                buffer = (id + " Y " + receiver + text).c_str();
+            }
+            else {
+                buffer = (id + " N " + "*" + " " + text).c_str();
+            }
+            send(client_sock, buffer, strlen(buffer), 0);
+        }
+        isServerConnected = false;
+        th2.join();
+        closesocket(client_sock);
+    }
+    WSACleanup();
+    return 0;
+}
